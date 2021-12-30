42276353730d646d6a06643fd2035f83e1ee81f4
Arthur Xavier <arthxvrr@outlook.com>
2021-12-30 18:52:21 -0300

    Altera linha 9 sinal de +

--- desafios-resolução/d010/script.js
+++ desafios-resolução/d010/script.js
@@ -6,7 +6,7 @@ function calcular() {
     var texto = document.getElementById('texto')
     var res = document.getElementById('res')
     texto.innerText = `Resolvendo Bhaskara`
-    res.innerHTML = `A equação atual é <strong>${a}x<sup>2</sup>+${b}x + 2 = 0</strong>
+    res.innerHTML = `A equação atual é <strong>${a}x<sup>2</sup> + ${b}x + 2 = 0</strong>
 <p>O cálculo realizado será <strong>&Delta; = ${b}<sup>2</sup> - 4 . ${a} . ${c}</p></strong>
 O valor calculado foi <strong><span id="deltaspan"> &Delta; = ${delta}</span></strong>`
 }
